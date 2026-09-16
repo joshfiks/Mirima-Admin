fda9f5d45c8431318c1b93b219e41d7499da5ab1
joshfiks <joshfiks04@gmail.com>
2026-09-16 13:30:55 +0300

    Test Admin Firestore request reading

--- js/dashboard.js
+++ js/dashboard.js
@@ -1611,6 +1611,10 @@ async function getRequestsFromFirestore() {
     });
 }
 
+getRequestsFromFirestore().then(function (requests) {
+    console.log("Mirima Admin requests:", requests);
+});
+
 function getRequestsContent() {
 
     return `
