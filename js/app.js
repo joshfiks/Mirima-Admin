18edab5941be42c2d99bff2b0acc9f805bd163cb
joshfiks <joshfiks04@gmail.com>
2026-09-16 12:37:18 +0300

    Connect Admin to Firebase

--- js/app.js
+++ js/app.js
@@ -7,7 +7,18 @@
    Authentication → auth.js
    Dashboard     → dashboard.js
 */
+import { firebaseConfig } from "../firebase-config.js";
 
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 // =========================================================
 // IMPORT MODULES
