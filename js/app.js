9d9473d4b86392c4b08f7411d0fa2c190fd377fd
joshfiks <joshfiks04@gmail.com>
2026-09-16 12:49:48 +0300

    Replace Firebase imports with local db module
    Refactor Firebase imports to use local module.

--- js/app.js
+++ js/app.js
@@ -7,20 +7,7 @@
    Authentication → auth.js
    Dashboard     → dashboard.js
 */
-import { firebaseConfig } from "../firebase-config.js";
-
-import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
-import {
-    getFirestore
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+import { db } from "./firebase.js";
 // =========================================================
 // IMPORT MODULES
 // =========================================================
