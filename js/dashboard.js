293de1c81008519a1b0d3b991725dc63ed9ba9ed
joshfiks <joshfiks04@gmail.com>
2026-09-16 14:50:51 +0300

    Remove temporary section click test

--- js/dashboard.js
+++ js/dashboard.js
@@ -726,9 +726,7 @@ function renderReceptionSection(section, session) {
 
 
         case "requests":
-          
-          console.log("Guest Requests section opened");
-
+                   
             title.textContent =
                 "Guest Requests";
 
